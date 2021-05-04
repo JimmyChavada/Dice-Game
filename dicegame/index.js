1c7158c9f52463c8dd3d0052018075f1b69e76e6
JimmyChavada <77020385+JimmyChavada@users.noreply.github.com>
2021-05-04 12:08:39 -0400

    Add files via upload

---
+++ dicegame/index.js
@@ -0,0 +1,22 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+
+var randomImage = "images/dice" + randomNumber1 + ".png"; 
+var randomImage2 = "images/dice" + randomNumber2 + ".png"; 
+
+document.querySelector('.img1').setAttribute("src", randomImage);
+document.querySelector('.img2').setAttribute("src", randomImage2);
+
+
+if (randomNumber1 > randomNumber2) { 
+    document.querySelector('h1').innerHTML = "player 1 wins";  
+}
+else if (randomNumber2 > randomNumber1) { 
+    document.querySelector('h1').innerHTML = "player 2 wins";  
+}
+else if (randomNumber1 === randomNumber2 ) { 
+    document.querySelector('h1').innerHTML ="draw!"; 
+}
+
+console.log(randomNumber1 + " " + randomNumber2)
